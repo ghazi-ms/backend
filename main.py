6e6b237eab06e2ea84a9750f4f22cc04a96c92de
ghazi-ms <64924247+ghazi-ms@users.noreply.github.com>
2023-05-25 15:30:55 +0300

    Update main.py

--- main.py
+++ main.py
@@ -1,109 +1,48 @@
-import random
 from flask import Flask, jsonify
 import time
 from bs4 import BeautifulSoup
 import requests
+import re
 import pandas as pd
 import feedparser
-
+from classs import news
+import concurrent.futures
 
 pd.set_option('display.max_colwidth', 500)
-api_key = "AIzaSyAKY_4kNJ0xHBgVCE6k9ZgSX-njXno1BTQ"
-API_URL = "https://api-inference.huggingface.co/models/CAMeL-Lab/bert-base-arabic-camelbert-msa-ner"
-headers = {"Authorization": "Bearer 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"}
+api_key = "AIzaSyBhW8eFNBry5bIvER254Q7pv8zdqxIJ6S4"
+API_URL = "https://api-inference.huggingface.co/models/hatmimoha/arabic-ner"
+headers = {"Authorization": "Bearer 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"}
 
 app = Flask(__name__)
 
 
 @app.route('/')
+def index():
+    def extract(news_list):
+        for news_data in news_list:
+            url = news_data.get_link()
+            news_source = news_data.get_source()
+            response = requests.get(url)
+            soup = BeautifulSoup(response.content, "html.parser")
 
-# start of the code
+            if news_source == 'alghad':
+                article_section = soup.find("div", id="atricle-text")
+                article_section = re.sub(r'\s+', ' ', article_section.get_text())
+                news_data.set_description(article_section)
+            elif news_source == 'roya':
+                article_section = soup.find("div", id="readMore_text")
+                article_section = article_section.get_text()
+                article_section = "\n".join(
+                    [line.strip() for line in article_section.split("\n") if "اقرأ أيضاً" not in line.strip()])
+                article_section = re.sub(r'\s+', ' ', article_section)
+                news_data.set_description(article_section)
 
-def start():
-    the_word = ["الأعاصير", "إطلاق نار", "زلزال", "حوادث", "زلازل", "حريق", "إرهاب", "الجرائم", "بحادثي", "وفاتان",
-            "حرب", "إصابات", "بانفجار"]
-    Feed = feedparser.parse('https://www.royanews.tv/rss')  # connect to royas rss
+        extract_location(news_list)
 
-    DataList = []  # the list that will have the initial data of type news
-    ImportnatnList = []
+    def get_boundary_coordinates(place_name):
+        if place_name == "":
+            return None
 
-    for i in Feed.entries:
-        # take every element in the list and cut the title and link
-        t = str(i).split("author")[3]
-        t = t.split("title")[2]
-        links = t.split("base")[1]
-        links = links.split("href")[1]
-        links = links.split("}")[0]
-        links = links.split("'")[2]
-        title = t.split("value")[1]
-        title = title.split("}")[0]
-
-        title = title.split("'")[2]
-        DataList.append(news(title, links))  # add the title and link to a new news object
-
-    for data in DataList:
-        for word in the_word:
-            if data.GetTitle().__contains__(
-                    word):  # if the data retrived from the list of the object news titles has any word of the keywords
-                tmp_data = news(data.title, data.link)
-                if tmp_data not in ImportnatnList:
-                    ImportnatnList.append(news(data.title, data.link))  # add the object to the filterd list
-
-    if ImportnatnList:  # if there is a filtered list then call the extractor, and it's not empty
-        extract(ImportnatnList)
-    theJsonlist = {}
-
-    for newsObject in ImportnatnList:
-        locations = newsObject.Getlocation()  # gets the location of the news
-        if locations != "":
-            locations = locations.split(',')  # the data is like amman,zaraqa,psut, so it splits the locations
-            locations = list(dict.fromkeys(locations))
-            locations.remove('')
-            for location in locations:
-                newsObject.SetPoints(get_boundary_coordinates(location))  # extract the coordinates of the location word
-        theJsonlist[str(random.randrange(1, 55))] = newsObject.getIntoList()  # turn the objects of news into a json list
-
-    return jsonify(theJsonlist)
-
-
-def extract(important_list):
-        for newsObjectData in important_list:
-
-            url = newsObjectData.GetLink()  # get the objects link (the news link)
-
-            response = requests.get(url) # request the page
-            soup = BeautifulSoup(response.content, "html.parser") # open the page
-
-            # find all sections in the HTML
-            sections = soup.find_all("section")
-
-            # check if there are at least four sections
-            if len(sections) >= 4:
-                target_section = sections[4]
-                target_section_contents = target_section.get_text()
-                target_section_contents = "\n".join(
-                    [line.strip() for line in target_section_contents.split("\n") if line.strip()])
-                newsObjectData.Setdescription(target_section_contents) # sets the description of the news object
-
-            ExtractLocation(important_list) # calls the extract location to extract the location from the description
-
-
-def extract_static(url):
-        response = requests.get(url)
-        soup = BeautifulSoup(response.content, "html.parser")
-
-        # find all sections in the HTML
-        sections = soup.find_all("div", {"class": "article"})
-
-        # check if there are at least two sections
-
-        for data in sections:
-            second_section_contents = data.get_text()
-            second_section_contents = "\n".join(
-                [line.strip() for line in second_section_contents.split("\n") if line.strip()])
-
-
-def get_boundary_coordinates(place_name):
         geocode_url = f"https://maps.googleapis.com/maps/api/geocode/json?address={place_name}&key={api_key}"
         response = requests.get(geocode_url)
         response_json = response.json()
@@ -117,97 +56,78 @@ def get_boundary_coordinates(place_name):
             viewport = geometry["viewport"]
             southwest = viewport["southwest"]
             northeast = viewport["northeast"]
-            boundary_coordinates = [(southwest["lat"], southwest["lng"]), (northeast["lat"], northeast["lng"])]
+            boundary_coordinates = [(northeast["lat"], southwest["lng"]), (northeast["lat"], northeast["lng"]),
+                                    (southwest["lat"], northeast["lng"]), (southwest["lat"], southwest["lng"])]
         else:
             southwest = bounds["southwest"]
             northeast = bounds["northeast"]
-            boundary_coordinates = [(southwest["lat"], southwest["lng"]), (northeast["lat"], northeast["lng"]),
-                                    (northeast["lat"], southwest["lng"]), (southwest["lat"], northeast["lng"])]
+            boundary_coordinates = [(northeast["lat"], southwest["lng"]), (northeast["lat"], northeast["lng"]),
+                                    (southwest["lat"], northeast["lng"]), (southwest["lat"], southwest["lng"])]
         return boundary_coordinates
 
-def GetDataAndDescription(payload):
+    def get_data_and_description(payload):
         response = requests.post(API_URL, headers=headers, json=payload)
-        estimatedTime = 0
-        if 'estimated_time' in response:
-            estimatedTime = response['estimated_time']
+        estimated_time = 0.0
+        if 'estimated_time' in response.text:
+            estimated_time = response.json()['estimated_time']
 
-        if estimatedTime > 0:
-
-            time.sleep(estimatedTime)
+        if estimated_time > 0:
+            time.sleep(estimated_time)
             response = requests.post(API_URL, headers=headers, json=payload)
 
         return response.json()
 
-def ExtractLocation(important_list):
-        theLocation = ""
-
-        for newsData in important_list:
-            if newsData.Getdescription() != '': # check if there is a description
-                response = GetDataAndDescription(newsData.Getdescription()) # send the description to the function
-
+    def extract_location(news_list):
+        country_list = ["الاردن","أفغانستان","الجزائر","البحرين","بنغلاديش","بوتان","البرازيل","بروناي","بلغاريا","بوركينا فاسو","بوروندي","كمبوديا","الكاميرون","الرأس الأخضر","جمهورية أفريقيا الوسطى","تشاد","الصين","كولومبيا","جزر القمر","جمهورية الكونغو","جمهورية الكونغو الديمقراطية","كوستاريكا","كوت ديفوار","كرواتيا","كوبا","قبرص","التشيك","الدنمارك","جيبوتي","دومينيكا","جمهورية الدومينيكان","تيمور الشرقية","الإكوادور","مصر","السلفادور","غينيا الإستوائية","إريتريا","إستونيا","إثيوبيا","فيجي","فنلندا","فرنسا","الغابون","غامبيا","جورجيا","ألمانيا","غانا","اليونان","غرينادا","غواتيمالا","غينيا","غينيا-بيساو","غيانا","هايتي","هندوراس","المجر","آيسلندا","الهند","إندونيسيا","إيران","العراق","جمهورية أيرلندا","إسرائيل","إيطاليا","جامايكا","اليابان","الأردن","كازاخستان","كينيا","كيريباتي","كوريا الشمالية","كوريا الجنوبية","الكويت","قرغيزستان","لاوس","لاتفيا","لبنان","ليسوتو","ليبيريا","ليبيا", "ليختنشتاين","ليتوانيا","لوكسمبورغ","مدغشقر","مالاوي","ماليزيا","جزر المالديف","مالي","مالطا","جزر مارشال","موريتانيا","موريشيوس","المكسيك","مايكرونيزيا","مولدوفا","موناكو","منغوليا","الجبل الأسود","المغرب","موزمبيق","ميانمار","ناميبيا","ناورو","نيبال","هولندا","نيوزيلندا","نيكاراجوا","النيجر","نيجيريا","جزيرة النورفولك","مقدونيا الشمالية","النرويج","عمان","باكستان","بالاو","بنما","بابوا غينيا الجديدة","باراغواي","بيرو","الفلبين","بولندا","البرتغال","قطر","رومانيا","روسيا","رواندا","سانت كيتس ونيفيس","سانت لوسيا","سانت فينسنت والغرينادين","ساموا","سان مارينو","ساو تومي وبرينسيبي","المملكة العربية السعودية","السنغال","صربيا","سيشل","سيراليون","سنغافورة","سلوفاكيا","سلوفينيا","جزر سليمان","الصومال","جنوب إفريقيا","جنوب السودان","إسبانيا","سريلانكا","السودان","سورينام","سوازيلاند","السويد","سويسرا","سوريا","تايوان","طاجيكستان","تنزانيا","تايلاند","توغو","تونجا","ترينداد وتوباغو", "تركيا","تركمانستان","توفالو","أوغندا","أوكرانيا","الإمارات العربية المتحدة","المملكة المتحدة","الولايات المتحدة الأمريكية","أوروغواي","أوزبكستان","فانواتو","فنزويلا","فيتنام","اليمن","زامبيا","زيمبابوي","أمريكا"]
+        provinces = ["عجلون", "العقبة", "الزرقاء", "السلط", "جرش", "الكرك", "معان", "المفرق", "مادبا", "عمان",
+                     "الطفيلة", "إربد"]
+        for news_data in news_list:
+            if news_data.get_description() != '':
+                response = get_data_and_description(news_data.get_description())
                 for item in response:
                     if isinstance(item, dict) and 'entity_group' in item:
-                        if item['entity_group'] == 'LOC':
-                            if item['entity_group'] == 'LOC':
-                                theLocation = theLocation + item['word'] + "," # append the extracted locations
+                        if item['entity_group'] == 'LOCATION' and \
+                                item['word'] not in news_data.get_location() and \
+                                '#' not in item['word'] and \
+                                item['word'] not in country_list and\
+                                item['word'] not in provinces and item['score'] >= 0.80:
+                            news_data.add_location(item['word'])
 
-            if theLocation != "":
-                newsData.SetLocation(theLocation) # add the list of locations
-                newsData.SettimeStamp(time.strftime("%m/%d/%Y, %H:%M:%S", time.localtime())) # give the news a time stamp
-            theLocation = ""
+    print("Request received")
+    the_word = ["الأعاصير", "إطلاق نار", "زلزال", "حوادث", "زلازل", "حريق", "إرهاب", "الجرائم", "التطورات", "حرب",
+                "إصابات", "بانفجار", "حادث", "إغلاق طريق", "فاجعة", "إصابة"]
+    news_websites = [['https://www.royanews.tv/rss', 'roya'], ['https://www.alghad.com/rss', 'alghad']]
+    data_list = []
+    important_list = []
 
+    with concurrent.futures.ThreadPoolExecutor() as executor:
 
+        for website in news_websites:
+            feed = feedparser.parse(website[0])
+            for entry in feed.entries:
+                data_list.append(news(entry['title'], entry['id'], website[1], entry['updated']))
 
-class news:
+        for data in data_list:
+            for word in the_word:
+                if data.get_title().__contains__(word):
 
-    def __init__(self, title, link):
-        self.title = title
-        self.link = link
-        self.description = ""
-        self.location = ""
-        self.timeStamp = time.strftime("%m/%d/%Y, %H:%M:%S", time.localtime())
-        self.points=[]
-    def __str__(self):
-        return "the title is :" + self.title + ",\n the link is:" + self.link + "\n the Description\n" + self.description + "\n the loc:" + self.location + "\n the time:" + self.timeStamp
+                    if data not in important_list:
+                        important_list.append(data)
 
-    def __eq__(self, other):
-        if isinstance(other,self.__class__):
-            return self.title == other.title
-        return False
-    def getIntoList(self):
-        return {
-        "id": 'id'+str(random.randrange(1,99999999)),
-        "title": self.title,
-        "description": self.description,
-        "Coordinates": self.points,
-        "Locations":self.location,
-        "timeStamp": self.timeStamp
-        }
-    def SetPoints(self,pointss):
-        self.points=pointss
-    def GetTitle(self):
-        return str(self.title)
+        executor.map(extract, [important_list])  # Concurrently extract data for each news object
 
-    def GetLink(self):
-        return str(self.link)
+    the_json_list = []
+    with concurrent.futures.ThreadPoolExecutor() as _:
+        for news_object in important_list:
+            locations = news_object.get_location()
+            boundary = get_boundary_coordinates(locations)
 
-    def Getlocation(self):
-        return str(self.location)
+            if boundary is not None:
+                news_object.set_points(boundary)
+                the_json_list.append(news_object.to_dict())
 
-    def SetLocation(self, Loc):
-        self.location = Loc
+    return jsonify(the_json_list)
 
-    def GettimeStamp(self):
-        return str(self.timeStamp)
-
-    def SettimeStamp(self, timestamp):
-        self.timeStamp = timestamp
-
-    def Setdescription(self, description):
-        self.description = description
-
-    def Getdescription(self):
-        return str(self.description)
 
 if __name__ == '__main__':
-    app.run(debug=True)
+    app.run(debug=True, host="0.0.0.0")
